1876dcb789655d3c4c9b95f17ed14e3743b02540
Luca Brügger <52595924+LBruegger@users.noreply.github.com>
2021-01-12 10:38:28 +0100

    Feature register (#56)
    Implement Register ability

--- frontend/src/app/components/auth/register/register.component.ts
+++ frontend/src/app/components/auth/register/register.component.ts
@@ -1,14 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { UserService } from '../../../service/user.service';
+import { Router } from '@angular/router';
+import User from '../../../models/user';
+import {throwError} from 'rxjs';
+import {ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  registerFormGroup = this.formBuilder.group({
+    username: this.formBuilder.control('', [Validators.required]),
+    email: this.formBuilder.control('', [Validators.email]),
+    password: this.formBuilder.control('', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,15}$')])
+  });
 
-  constructor() { }
+  error: HttpErrorResponse;
 
-  ngOnInit() {}
+  constructor(private userService: UserService,
+              private formBuilder: FormBuilder,
+              private router: Router,
+              private toastController: ToastController) {
+  }
 
+  register() {
+    const userToBeRegistered = new User();
+    userToBeRegistered.password = this.registerFormGroup.get('password').value;
+    userToBeRegistered.username = this.registerFormGroup.get('username').value;
+    userToBeRegistered.email = this.registerFormGroup.get('email').value;
+    this.userService
+        .registerUser(userToBeRegistered)
+        .subscribe(
+            async user => {
+                console.log(user);
+                this.userService.setUser(user as User);
+                await this.router.navigateByUrl('/');
+              },
+            async error => {
+              await this.handleError(error);
+            }
+        );
+  }
+
+  async handleError(error: HttpErrorResponse) {
+    const toast = await this.toastController.create({
+      message: error.error.detail,
+      duration: 2000
+    });
+    await toast.present();
+    return throwError(
+        'Something bad happened; please try again later.');
+  }
 }
